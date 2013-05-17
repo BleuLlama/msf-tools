2896f449fe05930649dcf10db4c51da3661eb764
Scott Lawrence <sdl@clyde.(none)>
2013-05-17 16:21:57 -0400

    LFN-8.3 finder

--- msf-dump.cpp
+++ msf-dump.cpp
@@ -11,6 +11,8 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#include <dirent.h> /* directory stuff */
+
 
 class VoiceFile {
 
@@ -81,6 +83,8 @@ public:
 	bool Valid( void ) { return valid; }
 public:
 	bool IsValidDir( std::string _path );
+	long FileSize( std::string _path );
+
 	void ConfigurePath( std::string _path );
 	std::string LFNFrom83( std::string _path, std::string eightthree );
 
@@ -132,8 +136,86 @@ void VoiceFile::ConfigurePath( std::string _path )
 	std::cout << "      MSF File is " << vrMSFFile << std::endl;
 }
 
+
+long VoiceFile::FileSize( std::string _path )
+{
+	struct stat sb;
+	if (stat(_path.c_str(), &sb) == 0 )
+	{
+		return (long) sb.st_size;
+	}
+	return 0;
+}
+
+// LFNFrom83
+// 	determine the long filename associated with the 8.3 pased in
+// 	the problem is that the lfn through a regular algo could map to multiple names
+// 	since we won't have a lot of files, let's just dumb-match it
+//
+//	so first, we check filesizes
+//	for alike-sizes, we check a few bytes at 0x800 bytes in, where files tend to differ.
 std::string VoiceFile::LFNFrom83( std::string _path, std::string eightthree )
 {
+	std::string e3_fullpath( _path );
+	e3_fullpath.append( eightthree );
+	long e3_size = this->FileSize( e3_fullpath );
+
+	// okay. let's now search the directory for the LFN with the same size.
+
+	DIR * d = opendir( _path.c_str() );
+	struct dirent * entry;
+	int foundcount = 0;
+	std::string foundname( "" );
+	if( d ) {
+
+		while(( entry = readdir( d )) != NULL ) {
+			std::string lfn_fullpath( _path );
+			lfn_fullpath.append( entry->d_name );
+			long lfn_size = this->FileSize( lfn_fullpath );
+
+			if( lfn_size == e3_size ) {
+				// okay. it's the right size.  let's compare
+				// bytes and check to see that they are the same
+				
+				FILE * fp_83 = fopen( e3_fullpath.c_str(), "rb" );
+				FILE * fp_lfn = fopen( lfn_fullpath.c_str(), "rb" );
+
+				if( fp_83 && fp_lfn ) {
+					int diffs = 0;
+
+					// skip in a bit
+					fseek( fp_83, 0x800, SEEK_SET );
+					fseek( fp_lfn, 0x800, SEEK_SET );
+					
+					// check 0x100 bytes.  On MP3s, thes will be different.
+					// with these files, it should be enough.
+					for( int b=0 ; b<0x100 ; b++ ) {
+						int a = fgetc( fp_83 );
+						int b = fgetc( fp_lfn );
+						if( a != b ) diffs++;
+					}
+					
+					fclose( fp_83 );
+					fclose( fp_lfn );
+
+					if( diffs == 0 ) {
+						foundcount++;
+						foundname.assign( entry->d_name );
+					}
+				}
+
+			}
+		}
+
+
+		closedir( d );
+	}
+
+	// okay. there was a match. return it.
+	if( foundcount >= 1 ) {
+		return foundname;
+	}
+
 	return _path + eightthree;
 }
 
